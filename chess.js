f9fbc0ba4813dd9287e2bbee8de67438d7cdc7e4
Jesús Marín Buschbeck <252979968+jeshuamb@users.noreply.github.com>
2026-09-23 23:31:21 -0400

    Fix syntax error in chess board generation

--- chess.js
+++ chess.js
@@ -4,7 +4,7 @@ for (let i = 0; i < 8; i++){
     for (let j = 0; j < 8; j++){
         if ((i + j) % 2 == 0){
             tablero += " ";
-        } else { tablero += "#" }
+        } else { tablero += "#"; }
     }
 
     tablero += "\n";
